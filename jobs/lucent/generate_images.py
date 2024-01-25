eed02759ff61948bd47673f6c3e5c186aafe366f
SoulOfAkuma <allertmika@gmail.com>
2024-01-25 15:37:15 +0100

    adjusted a sign error

--- jobs/lucent/generate_images.py
+++ jobs/lucent/generate_images.py
@@ -16,7 +16,7 @@ def generate_images(model_name, image_size, thresholds, classes, device):
         index = classes[cls]['index']
 
         objective = key_neuron_objective(block, index)
-        div_obj = key_neuron_objective(block, index) + 1e-3 * transformer_diversity_objective(block)
+        div_obj = key_neuron_objective(block, index) - 1e-3 * transformer_diversity_objective(block)
         param_f = image_batch(image_size, batch_size=5, device=device)
 
         clear_result = render.render_vis(model, objective, thresholds=thresholds, 
